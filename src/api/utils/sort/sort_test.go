420e384a5c7d7c04a6fa43bd129236909acff7bf
alopez <alopez@switchfly.com>
2021-01-28 21:51:46 -0700

    refactoring getelements function

--- src/api/utils/sort/sort_test.go
+++ src/api/utils/sort/sort_test.go
@@ -38,14 +38,23 @@ func TestSortOrderIncreasing(t *testing.T) {
 
 }
 
+func getElements(n int) []int {
+	result := make([]int, n)
+	j := 0
+	for i := n - 1; i > 0; i-- {
+		result[j] = i
+		j++
+	}
+	return result
+}
 func BenchmarkBubbleSort(b *testing.B) {
-	elements := []int{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}
+	elements := getElements(100000)
 	for i := 0; i < b.N; i++ {
 		BubbleSort(elements)
 	}
 }
 func BenchmarkSort(b *testing.B) {
-	elements := []int{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}
+	elements := getElements(100000)
 	for i := 0; i < b.N; i++ {
 		Sort(elements)
 	}
