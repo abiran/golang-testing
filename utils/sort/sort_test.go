53d3c02a0abf7deddd548df3a38f5e7efb163209
alopez <alopez@switchfly.com>
2021-01-28 20:18:41 -0700

    adding bubble sort test

---
+++ utils/sort/sort_test.go
@@ -0,0 +1,22 @@
+package sort
+
+import (
+	"testing"
+)
+
+func TestBubbleSortOrderDESC(t *testing.T) {
+	// Init step
+	elements := []int{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}
+
+	//Execution
+	BubbleSort(elements)
+
+	//Validation
+	if elements[0] != 9 {
+		t.Error("first element should be 9")
+	}
+	if elements[len(elements)-1] != 0 {
+		t.Error("last element should be 0")
+	}
+
+}
