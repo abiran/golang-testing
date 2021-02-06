21d0228ee7ca0f86197e3d9d26b47580a3883456
alopez <alopez@switchfly.com>
2021-02-05 19:47:12 -0700

    Refactoring test to avoid using service and provider (form integration to unit test)

--- src/api/controllers/location_controller_test.go
+++ src/api/controllers/location_controller_test.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"encoding/json"
+	"github.com/abiran/golang-testing/src/api/domain/locations"
+	"github.com/abiran/golang-testing/src/api/services"
 	"github.com/abiran/golang-testing/src/api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"github.com/mercadolibre/golang-restclient/rest"
@@ -12,19 +14,28 @@ import (
 	"testing"
 )
 
+var (
+	getCountryFunc func(countryId string) (*locations.Country, *errors.ApiError)
+)
+
 func TestMain(m *testing.M) {
 	rest.StartMockupServer()
 	os.Exit(m.Run())
 }
 
+type locationsServiceMock struct {
+}
+
+func (*locationsServiceMock) GetCountry(countryId string) (*locations.Country, *errors.ApiError) {
+	return getCountryFunc(countryId)
+}
+
 func TestGetCountryNotFound(t *testing.T) {
-	rest.FlushMockups()
-	rest.AddMockups(&rest.Mock{
-		URL:          "https://api.mercadolibre.com/countries/US",
-		HTTPMethod:   http.MethodGet,
-		RespHTTPCode: http.StatusNotFound,
-		RespBody:     `{"message": "Country not found","error": "not_found","status": 404,"cause": []}`,
-	})
+	// mock LocationsService methods:
+	getCountryFunc = func(countryId string) (*locations.Country, *errors.ApiError) {
+		return nil, &errors.ApiError{Status: http.StatusNotFound, Message: "Country not found"}
+	}
+	services.LocationsService = &locationsServiceMock{}
 	response := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(response)
 	c.Request, _ = http.NewRequest(http.MethodGet, "", nil)
